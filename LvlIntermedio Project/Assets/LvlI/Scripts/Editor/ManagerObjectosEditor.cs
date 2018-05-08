498a098536a7b1e70ecb0c0d43bdb2585b26e3ae
Moon Antonio <pinchodev91@gmail.com>
2018-05-09 00:51:15 +0200

    Navigation

---
+++ LvlIntermedio Project/Assets/LvlI/Scripts/Editor/ManagerObjectosEditor.cs
@@ -0,0 +1,48 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// ManagerObjetosEditor.cs (08/05/2018)											\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Herramienta													\\
+// Fecha Mod:		08/05/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEditor;
+#endregion
+
+namespace LvlI
+{
+	/// <summary>
+	/// <para>Herramienta.</para>
+	/// </summary>
+	public class ManagerObjectosEditor : EditorWindow
+	{
+		#region Menu
+		[MenuItem("Creador/Objeto")]
+		public static void Windows()
+		{
+			GetWindow(typeof(ManagerObjectosEditor), true, "Creador de Objetos");
+		}
+		#endregion
+
+		#region Eventos
+		public void OnEnable()
+		{
+			minSize = new Vector2(300, 53);
+			maxSize = new Vector2(4000, 53);
+		}
+		#endregion
+
+		#region GUI
+		private void OnGUI()
+		{
+			if (GUILayout.Button("Crear Objeto"))
+			{
+				AssetDatabase.CreateAsset(new Objeto(), "Assets/LvlI/Resources/Objeto.asset");
+			}
+		}
+		#endregion
+	}
+}
---
+++ LvlIntermedio Project/Assets/LvlI/Scripts/MotorEnemigo.cs
@@ -0,0 +1,38 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// MotorEnemigo.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEngine.AI;
+#endregion
+
+namespace LvlI
+{
+	public class MotorEnemigo : MonoBehaviour
+	{
+		#region Variables Publicas
+		public NavMeshAgent agente;
+		public Transform objetivo;
+		#endregion
+
+		#region Inicializaciones
+		private void Start()
+		{
+			if (agente == null) agente = this.gameObject.GetComponent<NavMeshAgent>();
+		}
+		#endregion
+
+		#region Actualizadores
+		private void Update()
+		{
+			agente.SetDestination(objetivo.position);
+		}
+		#endregion
+	}
+}
